1bc11fe23551b7ff99522ea1f89647d13ded8f76
Nandita <g00463584@atu.ie>
2026-03-18 20:55:13 +0000

    Added API key and updated server to display data every 20 seconds to Thingspeak server code.

---
+++ Code_API/secrets.h
@@ -0,0 +1,8 @@
+// Use this file to store all of the private credentials 
+// and connection details
+
+#define SECRET_SSID "Electronics"		// replace MySSID with your WiFi network name
+#define SECRET_PASS "TeriyakiPoulet!"	// replace MyPassword with your WiFi password
+
+#define SECRET_CH_ID 3297688  			// replace 0000000 with your channel number
+#define SECRET_WRITE_APIKEY "L0P34Y6RWV20VL7S"   // replace XYZ with your channel write API Key
